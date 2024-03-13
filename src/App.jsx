3b05292ea3fdd138c138b95f40585f97458698d1
EMON <mamudmdemon@gmail.com>
2024-03-13 11:58:52 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-4xl font-bold bg-red-200 p-4'>Knowledge Cafe</h1>
+     
+    </>
+  )
+}
+
+export default App
